1096a96efe20283aafa0dc89f43588f6b1e5831e
rubens_scotti <scotti.rubens@gmail.com>
2024-05-01 10:14:37 -0300

    SellersController

---
+++ SalesWebApp/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebApp.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
